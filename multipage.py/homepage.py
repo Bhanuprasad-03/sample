77033b4481e66b8b6f78fb711abf695ba4c68708
Bhanuprasad-03 <165460748+Bhanuprasad-03@users.noreply.github.com>
2024-03-31 15:45:13 +0530

    Add files via upload

---
+++ multipage.py/homepage.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.title("Homepage")
+
+st.text("this is homepage")
+st.video("angry_coder.mp4")
---
+++ multipage.py/pages/profile.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.title("profile")
+
+st.text("this is profile page")
+st.image("hari.jpg")
