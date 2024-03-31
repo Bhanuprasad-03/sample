0ff99b2429d03813e5bad176075cc050c0a68678
Bhanuprasad-03 <165460748+Bhanuprasad-03@users.noreply.github.com>
2024-03-31 16:03:43 +0530

    Update profile.py

--- multipage.py/pages/profile.py
+++ multipage.py/pages/profile.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.title("profile")
 
 st.text("this is profile page")
-st.image("hari.jpg")
+st.image("https://imgs.search.brave.com/g4LgthjayeX5-JZo6LOAedlnrmBAYMRZmwnRyNiUm3U/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE2/OTQ1ODc5NzE0NjQt/MGMwM2M0NGUyODk5/P2l4bGliPXJiLTQu/MC4zJml4aWQ9TTN3/eE1qQTNmREI4TUh4/bFpHbDBiM0pwWVd3/dFptVmxaSHd4TUh4/OGZHVnVmREI4Zkh4/OGZBPT0mdz0xMDAw/JnE9ODA.jpeg")
